b1f205840572ec92c6d55998e6cb4879a057ce18
mobilesite <mobilesite@163.com>
2018-01-07 10:52:03 +0800

    替换掉html文件

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import './index.less';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('__APP_RENDER_NODE__'));
